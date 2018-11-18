ecc583c8f926440704eaa4d67369a783ee340eb4
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-18 02:16:22 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,19 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array
 end
 
 def retrieve_first_element_from_array(array)
